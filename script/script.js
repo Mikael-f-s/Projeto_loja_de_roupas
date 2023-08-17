6210dff36ab283aadcc5b3b714836ef4683f28de
Mike <mikaelsfernandes98@gmail.com>
2023-08-17 15:14:25 -0300

    release 1.0

--- script/script.js
+++ script/script.js
@@ -44,14 +44,18 @@ function productTager(product, id){
     cardProduct.append(figure, div)
     figure.append(img)
     div.append(button1, h4, p1, p2, button2)
+
+    
+
     button1.addEventListener('click', ()=>{showcase.attShowcase(button1.innerText, 'type')})
-    button2.addEventListener('click', ()=>{alert('test')})
+    button2.addEventListener('click', ()=>{cart.addToCart(productToCart(productTag))})
 
     let productTag = {
         id: id,
         product: product,
         card: cardProduct,
     }
+    
 
     return productTag
 }
@@ -87,6 +91,78 @@ let showcase = {
 
 showcase.attShowcase()
 
+
+
+           
+function productToCart(productTag){
+        //create html tags
+    let miniCard = document.createElement('div')
+    let figure = document.createElement('figure')
+    let img = document.createElement('img')
+    let div = document.createElement('div')
+    let h4 = document.createElement('h4')
+    let p = document.createElement('p')
+    let button = document.createElement('button')
+        //add class
+    miniCard.classList.add('mini-card', 'flex')
+    figure.classList.add('flex', 'center')
+    img.setAttribute('src', productTag.product.image)
+    img.setAttribute('alt', 'Product reference')
+    div.classList.add('flex', 'center')
+        //appends
+    miniCard.append(figure, div)
+    figure.append(img)
+    div.append(h4, p, button)
+        //add content
+    h4.innerText = productTag.product.name
+    p.innerText = `R$ ${productTag.product.price.toFixed(2)}`
+    button.innerText = 'Remover produto'
+        //add event listener
+    button.addEventListener('click', ()=>{cart.rmvToCart(cartItem.cartId)})
+        //to cart
+    let cartItem = {
+        productId : productTag.id,
+        cartId : cart.products.length,
+        product : productTag.product,
+        mCard : miniCard
+    }
+
+    return cartItem
+}
+            
+
+
+let cart = {
+    html : document.querySelector('#cart-itens'),
+    products : [],
+    addToCart : (cartItem)=>{
+        cart.products.push(cartItem)
+        cart.attCart()
+    },
+    rmvToCart : (cartId)=>{
+        cart.products.map((product, indx)=>{
+            if(product.cartId == cartId){
+                cart.products.splice(indx, 1)                
+            }
+        })
+        cart.attCart()
+    },
+    attCart : ()=>{
+        let totalItens = 0
+        let totalValue = 0
+        let totalItenshtml = document.querySelector('#cart-resume').children[0].children[1]
+        let totalValuehtml = document.querySelector('#cart-resume').children[1].children[1]
+        cart.html.innerHTML = ''
+        cart.products.map((cartItem)=>{
+            cart.html.append(cartItem.mCard)
+            totalValue += cartItem.product.price
+            totalItens ++
+        })
+        totalItenshtml.innerText = totalItens == 0 ? ' - ' : totalItens
+        totalValuehtml.innerText = totalValue == 0 ? ' - ' : `R$ ${totalValue.toFixed(2)}`
+    },
+}
+
 const menu = document.querySelector('#menu')
 const menuLi = menu.firstElementChild.children
 let menuButtons = []
@@ -98,7 +174,11 @@ menuButtons.map((button)=>{button.addEventListener('click',()=>{showcase.attShow
 
 const search = document.querySelector('#search')
 search.children[1].addEventListener('click', ()=>{showcase.attShowcase(search.children[0].value, 'name')})
-            
+
+const toggleCart = document.querySelector('.toggleCart')
+const cartButton = document.querySelector('#cart').children[0]
+
+cartButton.addEventListener('click', ()=>{toggleCart.classList.toggle('toggleCart')})
 
 
 
